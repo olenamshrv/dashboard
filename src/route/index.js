18210c811ab4bffb631b3bfccb6f2a96a497b04f
olena.mashurova <olena.mashurova@gmail.com>
2023-05-31 14:06:26 +0300

    Lesson flexbox passed

--- src/route/index.js
+++ src/route/index.js
@@ -204,7 +204,7 @@ router.get('/template-2', function (req, res) {
       {
         date: 'Oct 24',
         title: 'Latest post',
-        text: 'This is a wider card with supporting text below as a natural lead-in to additional content',
+        text: 'This is a wider card with supporting text below as a natural lead-in to additional content This is a wider card with supporting text below as a natural lead-in to additional content',
         category: [
           {
             id: 1,
@@ -246,6 +246,21 @@ router.get('/template-2', function (req, res) {
           {
             id: 4,
             text: 'Fashion',
+          },
+        ],
+      },
+      {
+        date: 'Oct 24',
+        title: 'Latest post',
+        text: 'This is a wider card with supporting text below as a natural lead-in to additional content This is a wider card with supporting text below as a natural lead-in to additional content',
+        category: [
+          {
+            id: 1,
+            text: 'World',
+          },
+          {
+            id: 3,
+            text: 'Hot',
           },
         ],
       },
